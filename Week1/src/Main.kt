f882472be0da6ab13cdce11f684e77e65ac14223
BK <bartozo.krol@gmail.com>
2020-05-29 15:31:59 +0200

    Added an example of the game inside the main method.

--- Week1/src/Main.kt
+++ Week1/src/Main.kt
@@ -1,3 +1,21 @@
 fun main() {
-    println("This is my main function")
+    println("Android Bootcamp Challenge one")
+    println("Blackjack \n")
+
+    val dealer = Deck()
+    val hand = mutableListOf<Card>()
+
+    println("Creating new deck...")
+    val deck = dealer.createDeck()
+
+    println("Drawing 2 cards...")
+    hand.addAll(dealer.dealHand(deck.toMutableList(), 2))
+
+    println("Evaluating hand...")
+    val total = dealer.evaluateHand(hand)
+    println(total)
+
+    println("Printing the results...")
+    dealer.printResults(hand, total)
+
 }
