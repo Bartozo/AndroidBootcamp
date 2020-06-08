bf4a5af549df58a2480694996dfc53239fef283e
BK <bartozo.krol@gmail.com>
2020-06-08 13:52:43 +0200

    Updated Shelter class with more functions

--- Week2/src/Shelter.kt
+++ Week2/src/Shelter.kt
@@ -1,1 +1,47 @@
-data class Shelter(val id: Int, val address: String, val phoneNumber: String)
+class Shelter(id: Int, address: String, phoneNumber: String) {
+    private val id: Int
+    private val address: String
+    private val phoneNumber: String
+    private val cats: MutableList<Cat> = mutableListOf()
+
+    init {
+        this.id = id
+        this.address = address
+        this.phoneNumber = phoneNumber
+    }
+
+    // Getters
+    fun getId(): Int {
+        return this.id
+    }
+
+    fun getAddress(): String {
+        return this.address
+    }
+
+    fun getPhoneNumber(): String {
+        return this.phoneNumber
+    }
+
+    fun getCats(): List<Cat> {
+        return this.cats
+    }
+
+    fun getCat(catId: Int): Cat? {
+        for (cat in cats) {
+            if (cat.catId == catId) {
+                return cat
+            }
+        }
+        return null
+    }
+
+    // Other
+    fun addCat(cat: Cat) {
+        cats.add(cat)
+    }
+
+    fun removeCat(cat: Cat) {
+        cats.remove(cat)
+    }
+}
