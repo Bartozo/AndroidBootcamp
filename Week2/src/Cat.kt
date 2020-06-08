1d2835cf19ebf3c4dfce5d6bf7a655ccbf26102c
BK <bartozo.krol@gmail.com>
2020-06-08 13:31:43 +0200

    Added Cat class and Sex enum class

---
+++ Week2/src/Cat.kt
@@ -0,0 +1,25 @@
+class Cat(name: String, breed: String, sex: Sex, shelterId: Int, catId: Int) {
+    val name: String
+    val breed: String
+    val sex: Sex
+    val shelterId: Int
+    val catId: Int
+    val sponsorships = listOf<String>()
+
+    init {
+        this.name = name
+        this.breed = breed
+        this.sex = sex
+        this.shelterId = shelterId
+        this.catId = catId
+    }
+
+    fun adoptCat(customerId: Int) {
+        println("Cat has been adopted by customer with id $customerId")
+    }
+
+    fun sponsorCat(customerId: Int) {
+        println("Cat has been sponsored by customer with id $customerId")
+    }
+
+}
---
+++ Week2/src/Sex.kt
@@ -0,0 +1,3 @@
+enum class Sex {
+    FEMALE, MALE
+}
