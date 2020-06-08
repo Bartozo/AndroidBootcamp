ea355e775ef44d6ea8d793e9347256fccdc8fe35
BK <bartozo.krol@gmail.com>
2020-06-08 12:08:28 +0200

    Added MenuItem class

---
+++ Week2/src/MenuItem.kt
@@ -0,0 +1,11 @@
+open class MenuItem(name: String, price: Float, id: Int) {
+    private val name: String
+    private val price: Float
+    private val id: Int
+
+    init {
+        this.name = name
+        this.price = price
+        this.id = id
+    }
+}
