79ab411177d9617374f29119c90029ce29ef4683
BK <bartozo.krol@gmail.com>
2020-06-14 12:36:16 +0200

    Added quotes

--- Week3/app/src/main/java/com/bartoszkrol/businesscard/MainActivity.kt
+++ Week3/app/src/main/java/com/bartoszkrol/businesscard/MainActivity.kt
@@ -25,6 +25,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Properties
+    private val quotes = mutableListOf<String>(
+        "\"Opportunities don't happen, you create them.\" --Chris Grosser",
+        "\"Try not to become a person of success, but rather try to become a person of value.\" --Albert Einstein",
+        "\"I have not failed. I've just found 10,000 ways that won't work.\" --Thomas A. Edison",
+        "\"The distance between insanity and genius is measured only by success.\" --Bruce Feirstein",
+        "\"If you can't explain it simply, you don't understand it well enough.\" --Albert Einstein",
+        "\"Success is the sum of small efforts, repeated day-in and day-out.\" --Robert Collier"
+        "\"The starting point of all achievement is desire.\" --Napoleon Hill"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
