c487f9e157762cf1aca757f9ce1964bd8fc6f2b1
BK <bartozo.krol@gmail.com>
2020-06-22 17:25:24 +0200

    Setting up data in detail activity.

--- Week4/app/src/main/java/com/bartoszkrol/mymovies/DetailActivity.kt
+++ Week4/app/src/main/java/com/bartoszkrol/mymovies/DetailActivity.kt
@@ -2,10 +2,29 @@ package com.bartoszkrol.mymovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        // Get data from the intent
+        val title = intent.getStringExtra(MoviesActivity.MOVIE_TITLE)
+        val date = intent.getStringExtra(MoviesActivity.MOVIE_DATE)
+        val summary = intent.getStringExtra(MoviesActivity.MOVIE_SUMMARY)
+        val poster = intent.getIntExtra(MoviesActivity.MOVIE_POSTER, -1)
+
+        // Setup UI with data
+        title_text_view.text = title
+        date_text_view.text = date
+        description_text_view.text = summary
+        // Check if poster has no defaultValue -1
+        if (poster != -1) {
+            movie_image_view.setImageResource(poster)
+        }
+
     }
+
 }
