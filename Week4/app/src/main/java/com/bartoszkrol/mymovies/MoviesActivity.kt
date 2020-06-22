4a2d039e9f6516b0269724092db481ede378c21b
BK <bartozo.krol@gmail.com>
2020-06-22 16:43:54 +0200

    Added method to start and pass data to new activity

--- Week4/app/src/main/java/com/bartoszkrol/mymovies/MoviesActivity.kt
+++ Week4/app/src/main/java/com/bartoszkrol/mymovies/MoviesActivity.kt
@@ -1,12 +1,19 @@
 package com.bartoszkrol.mymovies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_movies.*
 
 class MoviesActivity : AppCompatActivity(), MovieListAdapter.MovieClickListener {
+
+    companion object {
+        const val MOVIE_TITLE = "MOVIE_TITLE"
+        const val MOVIE_DATE = "MOVIE_DATE"
+        const val MOVIE_SUMMARY = "MOVIE_SUMMARY"
+        const val MOVIE_POSTER = "MOVIE_POSTER"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +27,12 @@ class MoviesActivity : AppCompatActivity(), MovieListAdapter.MovieClickListener 
      * starts a new activity to show more details about the selected movie
      */
     override fun movieItemClicked(movie: Movie) {
-
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra(MOVIE_TITLE, movie.title)
+        intent.putExtra(MOVIE_DATE, movie.date)
+        intent.putExtra(MOVIE_SUMMARY, movie.summary)
+        intent.putExtra(MOVIE_POSTER, movie.poster)
+        startActivity(intent)
     }
 
 }
