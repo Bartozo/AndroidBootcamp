4c14260c55fdfa4fb2bec63d1dcb2375a05fdb45
BK <bartozo.krol@gmail.com>
2020-06-30 09:57:21 +0200

    Created LoginPrefs which use PreferenceManager

---
+++ Week5/app/src/main/java/com/bartoszkrol/myanimals/model/LoginPrefs.kt
@@ -0,0 +1,24 @@
+package com.bartoszkrol.myanimals.model
+
+import android.content.Context
+import androidx.preference.PreferenceManager
+import com.bartoszkrol.myanimals.app.MyAnimalsApplication
+
+object LoginPrefs {
+
+    private const val LOGIN_SHARED_PREFS = "LOGIN_SHARED_PREFS"
+    private const val KEY_IS_LOGGED_IN = "KEY_IS_LOGGED_IN"
+
+    private fun sharedPrefs() = MyAnimalsApplication.getAppContext().getSharedPreferences(LOGIN_SHARED_PREFS, Context.MODE_PRIVATE)
+
+    /**
+     * set isLoggedIn value
+     */
+    fun setUserLoggedIn(isLoggedIn: Boolean) = sharedPrefs().edit().putBoolean(KEY_IS_LOGGED_IN, isLoggedIn).apply()
+
+    /**
+     * check if user is logged in or no't
+     */
+    fun isLoggedIn(): Boolean = sharedPrefs().getBoolean(KEY_IS_LOGGED_IN, false)
+
+}
