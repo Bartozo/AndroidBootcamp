dfce0a93f76e5b591500ffb4617cd577b9f75a08
BK <bartozo.krol@gmail.com>
2020-07-06 13:06:55 +0200

    Updated AnimalsViewModel to use coroutines

--- Week6/app/src/main/java/com/bartoszkrol/myanimals/viewmodel/AnimalsViewModel.kt
+++ Week6/app/src/main/java/com/bartoszkrol/myanimals/viewmodel/AnimalsViewModel.kt
@@ -2,18 +2,26 @@ package com.bartoszkrol.myanimals.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.bartoszkrol.myanimals.app.Injection
 import com.bartoszkrol.myanimals.model.Animal
+import kotlinx.coroutines.launch
 
-class AnimalsViewModel(application: Application): AndroidViewModel(application) {
+class AnimalsViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = Injection.provideAnimalRepository()
 
     fun getAnimals() = repository.getAnimals()
 
-    fun insert(animal: Animal) = repository.addAnimal(animal)
+    fun insert(animal: Animal) = viewModelScope.launch {
+        repository.addAnimal(animal)
+    }
 
-    fun removeAnimal(animal: Animal) = repository.removeAnimal(animal)
+    fun removeAnimal(animal: Animal) = viewModelScope.launch {
+        repository.removeAnimal(animal)
+    }
 
-    fun removeAnimals() = repository.removeAllAnimals()
+    fun removeAnimals() = viewModelScope.launch {
+        repository.removeAllAnimals()
+    }
 
 }
