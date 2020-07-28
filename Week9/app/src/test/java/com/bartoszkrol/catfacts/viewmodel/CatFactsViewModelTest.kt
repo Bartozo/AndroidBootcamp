12d502478f63551e112e0fabb4cb45cb6bb387f0
BK <bartozo.krol@gmail.com>
2020-07-28 15:11:59 +0200

    Added empty test class for viewmodel

---
+++ Week9/app/src/test/java/com/bartoszkrol/catfacts/viewmodel/CatFactsViewModelTest.kt
@@ -0,0 +1,28 @@
+package com.bartoszkrol.catfacts.viewmodel
+
+import org.junit.Before
+import org.junit.Test
+
+class CatFactsViewModelTest {
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @Test
+    fun getCatFacts() {
+
+    }
+
+    @Test
+    fun removeCatFacts() {
+
+    }
+
+    @Test
+    fun insertCatFacts() {
+
+    }
+
+}
