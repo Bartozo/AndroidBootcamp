97db3055aa3dc4418817cb5323712a1e2f1f86e7
BK <bartozo.krol@gmail.com>
2020-07-28 15:41:53 +0200

    Added tests for CatFactsViewModel

--- Week9/app/src/test/java/com/bartoszkrol/catfacts/viewmodel/CatFactsViewModelTest.kt
+++ Week9/app/src/test/java/com/bartoszkrol/catfacts/viewmodel/CatFactsViewModelTest.kt
@@ -2,6 +2,8 @@ package com.bartoszkrol.catfacts.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.bartoszkrol.catfacts.database.RoomRepository
+import com.bartoszkrol.catfacts.model.CatFact
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -12,6 +14,12 @@ import org.mockito.MockitoAnnotations
 class CatFactsViewModelTest {
 
     private lateinit var catFactsViewModel: CatFactsViewModel
+
+    private val catFacts = mutableListOf<CatFact>(
+        CatFact("1","this is first fact about the cats", null, 1),
+        CatFact("2","this is second fact about the cats", null, 4),
+        CatFact("3","this is third fact about the cats", null, 3)
+    )
 
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
@@ -24,21 +32,33 @@ class CatFactsViewModelTest {
         MockitoAnnotations.initMocks(this)
 
         catFactsViewModel = CatFactsViewModel(repository)
+
+        // insert 3 CatFacts
+        catFactsViewModel.insertCatFacts(catFacts)
     }
 
     @Test
     fun getCatFacts() {
+        val liveData = catFactsViewModel.getCatFacts()
 
+        assertEquals(3, liveData.value?.size)
     }
 
     @Test
     fun removeCatFacts() {
+        // remove first CatFact from CatFacts
+        catFacts.removeAt(0)
+        val liveData = catFactsViewModel.getCatFacts()
 
+        assertEquals(2, liveData.value?.size)
     }
 
     @Test
     fun insertCatFacts() {
+        catFacts.add(CatFact("4","Another fact about the cats!", null, 5))
+        val liveData = catFactsViewModel.getCatFacts()
 
+        assertEquals(3, liveData.value?.size)
     }
 
 }
